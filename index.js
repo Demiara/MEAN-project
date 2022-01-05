794773a6b52761ca5467f2a8ef522f4d43f2e7f0
Tatyana Demiara <tatyana@kutalo.com>
2022-01-05 17:48:31 +0300

    feat(app.js): configure server

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Working',
-    });
-});
-app.listen(5000, () => console.log('@Server has been started'));
+app.listen(port, () => console.log(`@Server has been started on ${port}`));
